de01db1bcf4c3685dab5ef4e39f25b7f0345b198
mohammedboreny <Mohammed.boreny@gmail.com>
2023-01-10 16:42:45 +0300

    Auth routes modified

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Auth Routes
+
+Route::post('/register', [UserController::class, "register"]);
+Route::post('/login', [UserController::class, "login"]);
+Route::post('/updateUser', [UserController::class, "update_user"]);
+Route::post('/updatePassword', [UserController::class, "update_password"]);
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::post('/logout', [UserController::class, "logout"]);
 });
